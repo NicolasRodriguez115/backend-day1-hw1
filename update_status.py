34de32903f903002097864dade40f1b691d3c977
NicolasRodriguez115 <nr115935@gmail.com>
2024-06-19 00:02:27 -0400

    Finished the ticket application with an option to sort by status

--- update_status.py
+++ update_status.py
@@ -1,2 +1,6 @@
+from main import service_tickets
+from view_tickets import tickets_list
 def status_update():
-    pass
+    tickets_list()
+    select_ticket = input("Which ticket do you want to close?\n").capitalize()
+    service_tickets[f"{select_ticket}"]["Status"] = "Closed"
--- view_tickets.py
+++ view_tickets.py
@@ -1,4 +1,15 @@
 from main import service_tickets
 def tickets_list():
-    for ticket, details in service_tickets.items():
-        print(f"{ticket}: {details}")
+        for ticket, details in service_tickets.items():
+            print(f"{ticket}: {details['Issue']}, {details['Status']}, {details['Customer']}")
+        while True:
+            sort = input("Do you want to sort the list by status? Y/N\n").lower()
+            if sort == "y" or "yes":
+                sorted_tickets = dict(sorted(service_tickets.items(), key=lambda item: item[1]['Status']))
+                for ticket, details in sorted_tickets.items():
+                    print(f"{ticket}: {details['Issue']}, {details['Status']}, {details['Customer']}")
+                    return
+            elif sort == "n" or "no":
+                return
+            else:
+                print("You didn't type a valid input. Try again after pressing 'enter'.\n")
